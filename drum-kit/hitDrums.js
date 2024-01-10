6ef41ba681749386255da9d76d03cb9d1cebb383
Lorena Infante <m.lorenainfanter@gmail.com>
2024-01-10 16:36:19 -0300

    added sound to keys in hitDrums

--- drum-kit/hitDrums.js
+++ drum-kit/hitDrums.js
@@ -1,8 +1,66 @@
-export function hitDrums(){
-    document.addEventListener('keydown', getTheKey);
+//import clap from './public/clap.wav';
+export function hitDrums() {
+  document.addEventListener("keydown", getTheKey);
 
-    function getTheKey(e){
-        console.log(e.code);
+  function getTheKey(e) {
+    const keyCode = e.code;
+    switch (keyCode) {
+      case "KeyA":
+        console.log(`KeyA`);
+        const clapSound = new Audio ('/clap.wav?url');
+        clapSound.play();
+        break;
+
+      case "KeyS":
+        console.log(`KeyS`);
+        const hitHatSound = new Audio ('/hihat.wav?url');
+        hitHatSound.play();
+        break;
+
+      case "KeyD":
+        console.log(`KeyD`);
+        const kickSound = new Audio ('/kick.wav?url');
+        kickSound.play();
+        break;
+
+      case "KeyF":
+        console.log(`KeyF`);
+        const openHatSound = new Audio ('/openhat.wav?url');
+        openHatSound.play();
+        break;
+
+      case "KeyG":
+        console.log(`KeyG`);
+        const boomSound = new Audio ('/boom.wav?url');
+        boomSound.play();
+        break;
+
+      case "KeyH":
+        console.log(`KeyH`);
+        const rideSound = new Audio ('/ride.wav?url');
+        rideSound.play();
+        break;
+
+      case "KeyJ":
+        console.log(`KeyJ`);
+        const snareSound = new Audio ('/snare.wav?url');
+        snareSound.play();
+        break;
+
+      case "KeyK":
+        console.log(`KeyK`);
+        const tomSound = new Audio ('/tom.wav?url');
+        tomSound.play();
+        break;
+
+      case "KeyL":
+        console.log(`KeyL`);
+        const tinkSound = new Audio ('/tink.wav?url');
+        tinkSound.play();
+        break;
+
+      default:
+        console.log(`Do nothing`);
     }
-
+  }
 }
--- drum-kit/main.js
+++ drum-kit/main.js
@@ -8,16 +8,16 @@ document.querySelector('#app').innerHTML = `
   <div>
     <h1>Drumkit</h1>
     <h2>Press any key to begin</h2>
-    <div class="container-btns">
-    <button data-id="clap" class="btn_sound">A <span class="text-yellow">clap</span></button>
-    <button data-id="hit-hat" class="btn_sound">S <span class="text-yellow">hit-hat</span></button>
-    <button data-id="kick" class="btn_sound">D <span class="text-yellow">kick</span></button>
-    <button data-id="open-hat" class="btn_sound">F <span class="text-yellow">open-hat</span></button>
-    <button data-id="ride" class="btn_sound">G <span class="text-yellow">ride</span></button>
-    <button data-id="snare" class="btn_sound">H <span class="text-yellow">snare</span></button>
-    <button data-id="tink" class="btn_sound">J <span class="text-yellow">tink</span></button>
-    <button data-id="tom" class="btn_sound">K <span class="text-yellow">tom</span></button>
-    <button data-id="boom" class="btn_sound">L <span class="text-yellow">boom</span></button>
+    <div id="buttons" class="container-btns">
+    <button id="KeyA" class="btn_sound">A <span class="text-yellow">clap</span></button>
+    <button id="KeyS" class="btn_sound">S <span class="text-yellow">hit-hat</span></button>
+    <button id="KeyD" class="btn_sound">D <span class="text-yellow">kick</span></button>
+    <button id="KeyF" class="btn_sound">F <span class="text-yellow">open-hat</span></button>
+    <button id="KeyG" class="btn_sound">G <span class="text-yellow">boom</span></button>
+    <button id="KeyH" class="btn_sound">H <span class="text-yellow">ride</span></button>
+    <button id="KeyJ" class="btn_sound">J <span class="text-yellow">snare</span></button>
+    <button id="KeyK" class="btn_sound">K <span class="text-yellow">tom</span></button>
+    <button id="KeyL" class="btn_sound">L <span class="text-yellow">tink</span></button>
     </div>
   </div>
 `

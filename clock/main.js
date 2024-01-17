0e890b8e93282504dd492fe5abd852c0c8835851
Lorena Infante <m.lorenainfanter@gmail.com>
2024-01-17 15:52:01 -0300

    vide architecture and created first CSS with clock and hourly- minute-hands

---
+++ clock/main.js
@@ -0,0 +1,20 @@
+import './style.css'
+//import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>CSS + JS CLOCK</h1>
+    <div class="clock-container">
+      <div id="id-clock" class="clock">
+        <div class="clock-hands-container">
+          <span id="bullet"></span>
+          <span id="hourly-hand"></span>
+          <span id="minute-hand"></span>
+          <span id="second-hand"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+`
+
+//setupCounter(document.querySelector('#counter'))

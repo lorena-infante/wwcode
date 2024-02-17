33a6a7beaa76b39c2b7df6e8b48d1765621989ca
Lorena Infante <m.lorenainfanter@gmail.com>
2024-02-17 13:48:45 -0300

    added flex-gallery images and some initial css on hover and widths

---
+++ flex-gallery/main.js
@@ -0,0 +1,20 @@
+import './style.css'
+//import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+<div class="container-imgs">
+<div class="row">
+  <div id="img-1" class="col-img col-img-1">
+  <div class="text-container">
+    <span>Hola</span>
+    <span>Chao</span>
+  </div></div>
+  <div id="img-2" class="col-img col-img-2"></div>
+  <div id="img-3" class="col-img col-img-3"></div>
+  <div id="img-4" class="col-img col-img-4"></div>
+  <div id="img-5" class="col-img col-img-5"></div>
+</div>
+</div>
+`
+
+//setupCounter(document.querySelector('#counter'))

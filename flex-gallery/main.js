d5ad862110faeafa8248e09a20c68c428952b91a
Lorena Infante <m.lorenainfanter@gmail.com>
2024-02-28 16:44:27 -0300

    added neon text, new text on the cards and updated css on hover

--- flex-gallery/main.js
+++ flex-gallery/main.js
@@ -1,20 +1,40 @@
-import './style.css'
+import "./style.css";
 //import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
+/*Let us firmly hold the profession of our faith without wavering, for He who promised is faithful. Hb.10:23*/
+document.querySelector("#app").innerHTML = `
 <div class="container-imgs">
 <div class="row">
   <div id="img-1" class="col-img col-img-1">
-  <div class="text-container">
-    <span>Hola</span>
-    <span>Chao</span>
-  </div></div>
-  <div id="img-2" class="col-img col-img-2"></div>
-  <div id="img-3" class="col-img col-img-3"></div>
-  <div id="img-4" class="col-img col-img-4"></div>
-  <div id="img-5" class="col-img col-img-5"></div>
+    <div class="text-container">
+      <span>Let us</span>
+      <span>firmly hold</span>
+    </div>
+  </div>
+  <div id="img-2" class="col-img col-img-2">
+    <div class="text-container">
+      <span>the profession</span>
+      <span>of our faith</span>
+  </div>
+  </div>
+  <div id="img-3" class="col-img col-img-3">
+    <div class="text-container">
+      <span>without wavering,</span>
+      <span>for He</span>
+    </div>
+  </div>
+  <div id="img-4" class="col-img col-img-4">
+    <div class="text-container">
+      <span>who promised</span>
+      <span>is faithful.</span>
+    </div>
+  </div>
+  <div id="img-5" class="col-img col-img-5">
+    <div class="text-container">
+      <span>Hb.10:23</span>
+    </div>
+  </div>
 </div>
 </div>
-`
+`;
 
 //setupCounter(document.querySelector('#counter'))
